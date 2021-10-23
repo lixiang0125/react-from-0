1b48aa3b92f5a43dad48a4b0b7f930e09a0f0743
lixiang <lixiang0125@gmail.com>
2021-10-23 18:20:10 +0800

    feat: add postcss

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -13,11 +13,43 @@ module.exports = {
       test: /\.(js|jsx|ts|tsx)$/,
       loader: 'babel-loader',
       exclude: /(node_modules|build)/,
+    }, {
+      test: /\.css$/,
+      include: path.src,
+      use: [
+        'style-loader',
+        'css-loader',
+      ],
+    }, {
+      test: /.(scss|sass)$/,
+      include: path.src,
+      use: [
+        'style-loader',
+        {
+          loader: 'css-loader',
+          options: {
+            modules: true,
+            importLoader: 2,
+          },
+        },
+        {
+          loader: 'postcss-loader',
+          options: {
+            postcssOptions: {
+              plugins: [
+                ['postcss-preset-env'],
+              ],
+            },
+          },
+        },
+        'sass-loader',
+      ],
     }],
   },
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx'],
   },
+  devtool: 'eval-cheap-module-source-map',
   devServer: {
     port: 8080,
     contentBase: path.join(__dirname, './dist'),
